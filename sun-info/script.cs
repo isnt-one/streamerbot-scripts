9c3e3a38dc34b6dc5fc0470dd5b8dc7a4d962d61
One <7042182+isnt-one@users.noreply.github.com>
2026-06-14 12:01:20 -0500

    Refactor sun info retrieval to use shared HttpClient
    Prevent port exhaustion by using an instanced HttpClient

--- sun-info/script.cs
+++ sun-info/script.cs
@@ -1,7 +1,9 @@
 using System;
-using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 /*
  * Description: This action uses geographical coordinates of a specified location to find times related to sun light.
@@ -27,41 +29,45 @@ class JsonResponse {
 }
 
 public class CPHInline {
+	private static readonly HttpClient _httpClient = new HttpClient{Timeout = TimeSpan.FromSeconds(30)};
+	
+	public void GetLocalizedTime(string timeZone, out string date) {
+		TimeZoneInfo timezone = TimeZoneInfo.FindSystemTimeZoneById(timeZone);
+        DateTime localizedTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezone);
+		date = localizedTime.ToString("yyyy-MM-d");
+	}
+
     public string ModifyTime(string time) {
         int last = time.LastIndexOf(":");
         return time.Remove(last, 3);
     }
 
-    public bool Execute() {
-        TimeZoneInfo timezone = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
-        DateTime localizedTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezone);
-        string date = localizedTime.ToString("yyyy-MM-d");
+	public void Dispose() {
+		_httpClient.Dispose();
+	}
+	
+	public bool Execute() {
+		this.GetLocalizedTime("Tokyo Standard Time", out string date);
+        string url = string.Format("https://api.sunrisesunset.io/json?lat={0}&lng={1}&timezone=JST&date={2}", "35.6762", "139.6503", date);
 
-        using(HttpClient client = new HttpClient()) {
-            string url = @"https://api.sunrisesunset.io";
-            url = string.Format("{0}/json?lat={1}&lng={2}&timezone=JST&date={3}", url, "35.6762", "139.6503", date);
+		HttpResponseMessage response = _httpClient.GetAsync(url).GetAwaiter().GetResult();
+		if(!response.IsSuccessStatusCode) {
+			CPH.SendMessage("Error: Could not get sun info");
+			return false;
+		}
 
-            HttpResponseMessage response = client.GetAsync(url).GetAwaiter().GetResult();
-            if (response.IsSuccessStatusCode) {
-                string responseBody = response.Content.ReadAsStringAsync().Result;
-                JsonResponse content = JsonConvert.DeserializeObject<JsonResponse>(responseBody);
+		string responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+		JsonResponse content = JsonConvert.DeserializeObject<JsonResponse>(responseBody);
 
-                Results results = content.results;
+		Results results = content.results;
+        string sunrise = ModifyTime(results.sunrise);
+        string dawn = ModifyTime(results.dawn);
+        string dusk = ModifyTime(results.dusk);
+        string sunset = ModifyTime(results.sunset);
+        string golden_hour = ModifyTime(results.golden_hour);
 
-                string sunrise = ModifyTime(results.sunrise);
-                string dawn = ModifyTime(results.dawn);
-                string dusk = ModifyTime(results.dusk);
-                string sunset = ModifyTime(results.sunset);
-                string golden_hour = ModifyTime(results.golden_hour);
-
-                string data = string.Format("Sun info for Tokyo: Sunrise: {0}, Sunset: {1}, Dawn: {2}, Dusk: {3}, Golden hour: {4}", sunrise, sunset, dawn, dusk, golden_hour);
-
-                CPH.SendMessage(data);
-            } else {
-                CPH.SendMessage("Failed to get sun data");
-            }
-        }
-
-        return true;
-    }
+		CPH.SendMessage($"Sun info for Tokyo: Sunrise: {sunrise}, Sunset: {sunset}, Dawn: {dawn}, Dusk: {dusk}, Golden hour: {golden_hour}");
+        
+		return true;
+	}
 }
